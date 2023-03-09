ccd8be2a5ef7a0b4409ce5e95045ddc81b00b599
MarkLee7925 <59748085+MarkLee7925@users.noreply.github.com>
2023-03-09 14:31:13 -0500

    Add files via upload

---
+++ Queries/ViewAllTables.sql
@@ -0,0 +1,26 @@
+-- View Base Tables
+
+-- Customers
+SELECT * FROM customers;
+
+-- Employees
+SELECT * FROM employees;
+
+-- Offices
+SELECT * FROM offices;
+
+-- Office Details
+SELECT * FROM officedetails;
+
+-- Orders
+SELECT * FROM orders;
+
+-- Payments
+SELECT * FROM payments;
+
+-- Product Lines
+SELECT * FROM productlines;
+
+-- Products
+SELECT * FROM products;
+
